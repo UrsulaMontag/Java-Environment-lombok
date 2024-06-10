a2ed68a23db4d23c72cc7e4fce927bd8cf15218e
ugla <montagu666@gmail.com>
2024-06-10 15:53:28 +0200

    Test lombok

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student = new Student();
+        Student student2 = new Student("test-id1", "Max", "Stuttgart", "B");
+        System.out.println("-------------------student empty and student filled------------");
+        System.out.println("Student1: " + student);
+        System.out.println("Student2: " + student2);
+
+        System.out.println("-------------------student SETTER------------");
+        student.setId("test-id2");
+        student.setName("Mathilde");
+        System.out.println("Student1: " + student);
+
+        Teacher teacher1 = new Teacher("test-id54", "Harald", "Java");
+        Teacher teacher2 = new Teacher("test-id89", "Siglinde", "Javascript");
+        System.out.println("-------------------teachers------------");
+        System.out.println("Teacher1: " + teacher1);
+        System.out.println("Teacher2: " + teacher2);
+
+        System.out.println("-------------------teacher wither------------");
+        Teacher teacher3 = teacher2.withId("test-id634");
+        System.out.println("Teacher3: " + teacher3);
+
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -3,11 +3,13 @@ package org.example;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Student {
+    @NonNull
     private String id;
     private String name;
     private String address;
